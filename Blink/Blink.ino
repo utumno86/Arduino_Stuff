c4a26686e6b6bcd2f495b6e3128e05e10ad9f511
Michael Nash <michael@spatialnetworks.com>
2018-02-15 20:38:29 -0500

    initial button code

--- Blink/Blink.ino
+++ Blink/Blink.ino
@@ -1,13 +1,13 @@
 void setup() {
   // put your setup code here, to run once:
-  pinMode(13, OUTPUT);
+  pinMode(8, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(13, HIGH); //turn the LED on (HIGH id the voltage level)
-  delay(5000);            //wait for a second
-  digitalWrite(13, LOW);  //turn the LED off
-  delay(5000);            //wait for a seond
+  digitalWrite(8, HIGH); //turn the LED on (HIGH id the voltage level)
+  delay(1000);            //wait for a second
+  digitalWrite(8, LOW);  //turn the LED off
+  delay(1000);            //wait for a seond
 }
