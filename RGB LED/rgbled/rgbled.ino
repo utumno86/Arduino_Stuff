59ad349434ca0ab496212e1e6f71681aa1862b5c
mnash <utumno86@gmail.com>
2018-02-24 23:15:44 -0500

    red green blue lights

--- RGB LED/rgbled/rgbled.ino
+++ RGB LED/rgbled/rgbled.ino
@@ -1,15 +1,27 @@
-#define RED_PIN 3;
+#define BLUE_PIN 3
+#define GREEN_PIN 5
+#define RED_PIN 9
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(3, OUTPUT);
+  //pin 3 = blue
+  //pin 5 = green
+  //pin 9 = red
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(BLUE_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(3, HIGH); //turn the LED on (HIGH id the voltage level)
+  digitalWrite(RED_PIN, HIGH); //turn the LED on (HIGH id the voltage level)
   delay(1000);            //wait for a second
-  digitalWrite(3, LOW);  //turn the LED off
-  delay(500);            //wait for a seond
+  digitalWrite(RED_PIN, LOW);  //turn the LED off
+  digitalWrite(BLUE_PIN, HIGH);
+  delay(1000);            //wait for a seond
+  digitalWrite(BLUE_PIN, LOW);  //turn the LED off
+  digitalWrite(GREEN_PIN, HIGH);
+  delay(1000);
+  digitalWrite(GREEN_PIN, LOW);
 }
