83b72a09b288cfb47ebeab59e2abb55ac54ad931
mnash <utumno86@gmail.com>
2018-02-24 23:04:39 -0500

    created a partition for rgb led

--- RGB LED/rgbled/rgbled.ino
+++ RGB LED/rgbled/rgbled.ino
@@ -1,16 +1,15 @@
+#define RED_PIN 3;
+
 void setup() {
   // put your setup code here, to run once:
-  //pin 3 = blue
-  //pin 5 = green
-  //pin 7 = red
-  pinMode(9, OUTPUT);
+  pinMode(3, OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(9, HIGH); //turn the LED on (HIGH id the voltage level)
+  digitalWrite(3, HIGH); //turn the LED on (HIGH id the voltage level)
   delay(1000);            //wait for a second
-  digitalWrite(9, LOW);  //turn the LED off
+  digitalWrite(3, LOW);  //turn the LED off
   delay(500);            //wait for a seond
 }
